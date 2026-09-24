acb3ec635a4e614f10d58c18dbc149dfb3945766
salatt69 <id9114546@gmail.com>
2026-09-24 12:05:25 +0300

    fix: overdrive shader deactivation time now actually matches cooldown time

--- ProjectSynth_VS/States/Synth/Metro/MetroCooldownState.cs
+++ ProjectSynth_VS/States/Synth/Metro/MetroCooldownState.cs
@@ -4,26 +4,28 @@ namespace ProjectSynth.States.Synth.Metro
 {
     public sealed class MetroCooldownState(float overdiveMeterValue) : BaseMetroState
     {
-        private float _cooldownTime;
+        private float _overdriveScaledTime;
         private readonly float _overdiveMeterValue = overdiveMeterValue;
 
         public override void OnEnter()
         {
             base.OnEnter();
 
-            _cooldownTime = _overdiveMeterValue * SynthValues.MetroCooldownOverdriveInfluenceCoefficient;
+            _overdriveScaledTime = _overdiveMeterValue * SynthValues.MetroCooldownOverdriveInfluenceCoefficient;
 
             float speed = (_metro.SpeedMult > 0f) ? _metro.SpeedMult : 2f;
-            _metro.cooldownSpeedMult = speed / _cooldownTime;
+            float cooldownSpeed = speed / _overdriveScaledTime;
+
+            _metro.cooldownSpeedMult = cooldownSpeed;
             _metro.cooldownStartedThisFrame = true;
 
-            shaderOverlay.SetActive(overdriveOverlayMaterial, false, _cooldownTime);
+            shaderOverlay.SetActive(overdriveOverlayMaterial, false, cooldownSpeed);
         }
 
         public override void FixedUpdate()
         {
             base.FixedUpdate();
-            if (fixedAge >= _cooldownTime)
+            if (fixedAge >= _overdriveScaledTime)
             {
                 outer.SetNextState(new MetroWaitForInputState());
             }
